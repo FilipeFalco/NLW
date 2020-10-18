05a76b82f0522fc4b96729006346aeeb4698b08c
Filipe Ferreira Falco <falcoefilipe@hotmail.com>
2020-10-17 20:07:28 -0400

    dia 4

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -4,19 +4,34 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 //create icon
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
+    iconUrl: "/images/map-marker.svg",
     iconSize: [58, 68],
     iconAnchor: [29, 68],
     popupAnchor: [170, 2]
 })
 
-//create popup overlay
-const popup = L.popup({
-    closeButton: false,
-    className: 'map-popup',
-    minWidth: 240,
-    minHeight:240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+function addMarker({id, name, lat, lng}) {
+    //create popup overlay
+    const popup = L.popup({
+        closeButton: false,
+        className: 'map-popup',
+        minWidth: 240,
+        minHeight:240
+    }).setContent(`${name} <a href="/orphanage?id=${id}"> <img src="/images/arrow-white.svg"> </a>`)
 
-L.marker([-27.2195499,-49.6466143], { icon }).addTo(map)
+    L.marker([lat,lng], { icon })
+    .addTo(map)
     .bindPopup(popup)
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+orphanagesSpan.forEach( orphanageElement => {
+    const orphanage = {
+        id: orphanageElement.dataset.id,
+        name: orphanageElement.dataset.name,
+        lat: orphanageElement.dataset.lat,
+        lng: orphanageElement.dataset.lng
+    }
+
+    addMarker(orphanage)
+})
